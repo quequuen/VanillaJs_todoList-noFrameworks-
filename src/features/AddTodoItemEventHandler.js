165fed7670097e239c02fc2c7563b6102ca4c968
quequuen <rkdusl2020@gmail.com>
2025-05-09 22:18:21 +0900

    Fix: addTodoItemEventHandler.js, searchTodoListEventHandler.js 수정    1. 하나의 기능을 하나의 함수가 하게 수정   2.AddTodoItemEventHansler.js->addTodoItemEventHandler.js 이름 수정

--- src/features/AddTodoItemEventHandler.js
+++ src/features/AddTodoItemEventHandler.js
@@ -1,28 +1,48 @@
 import { globalStore } from "../stores/globalStore";
+const setInputValues = () => {
+  const $date = document.getElementById("todoDate");
+  const $content = document.getElementById("todoContent");
+  return {
+    date: $date.value,
+    content: $content.value,
+    $date,
+    $content,
+  };
+};
+const getDate = () => new Date().toISOString().split("T")[0];
+
+const createTodoItem = (id, deadLine, content) => ({
+  id,
+  creation: getDate(),
+  deadLine,
+  isDone: "N",
+  content,
+});
+
+const setTodoStore = (newTodo) => {
+  const todos = globalStore.getState().posts;
+  globalStore.setState({ posts: [...todos, newTodo] });
+};
+
+const resetTodoForm = ($date, $content) => {
+  $date.value = getDate();
+  $content.value = "";
+};
 
 const addTodoItemEventHandler = (e) => {
   e.preventDefault();
   const todos = globalStore.getState().posts;
+  //setInputValues() 호출로 DOM 요소의 값과 그 자체를 각각 변수에 할당 후 리턴(구조 분해 할당 사용)
+  const { date, content, $date, $content } = setInputValues();
 
-  const $date = document.getElementById("todoDate");
-  const $content = document.getElementById("todoContent");
-  const today = new Date().toISOString().split("T")[0];
+  // 구조 분해 할당으로 가져온 date와 content를 createTodoItem의 인수로 주고 객체를 생성한 후 newTodo에 할당
+  const newTodo = createTodoItem(todos.length + 1, date, content);
 
-  const newTodo = {
-    id: todos.length + 1,
-    creation: today,
-    deadLine: $date.value,
-    isDone: "N",
-    content: $content.value,
-  };
-
-  globalStore.setState({
-    posts: [...todos, newTodo],
-  });
+  // newTodo 데이터 저장
+  setTodoStore(newTodo);
 
   // 입력 초기화
-  $date.value = today;
-  $content.value = "";
+  resetTodoForm($date, $content);
 };
 
 export default addTodoItemEventHandler;
--- src/features/searchTodoListEventHandler.js
+++ src/features/searchTodoListEventHandler.js
@@ -1,21 +1,28 @@
 import TodoItem from "../components/TodoItem";
 import { globalStore } from "../stores/globalStore";
 
+const getDate = () => new Date().toISOString().split("T")[0];
+
+const getFilterTodoDataBySearch = (todos, search) => {
+  return todos.filter(
+    (todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()) &&
+      todo.deadLine === getDate()
+  );
+};
+
+const renderTodoListBySearch = ($list, filterTodoData) => {
+  $list.innerHTML = filterTodoData.map((todo) => TodoItem(todo)).join("");
+};
+
 const searchTodoListEventHandler = (e) => {
-  // console.log("searchInput 작동");
   const todos = globalStore.getState().posts;
   const search = e.target.value;
   const $list = document.getElementById("todoList");
-  let today = new Date().toISOString().split("T")[0];
 
   if ($list) {
-    const filterTodoData = todos.filter(
-      (todo) =>
-        todo.content.toLowerCase().includes(search.toLowerCase()) &
-        (todo.deadLine === today)
-    );
-
-    $list.innerHTML = filterTodoData.map((todo) => TodoItem(todo)).join("");
+    const filterTodoData = getFilterTodoDataBySearch(todos, search);
+    renderTodoListBySearch($list, filterTodoData);
   }
 };
 
