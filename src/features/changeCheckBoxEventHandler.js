c5c76a998306931ded1a2312f5a18adf1e7bbeaf
quequuen <rkdusl2020@gmail.com>
2025-05-12 12:09:59 +0900

    Fix: changeCheckBoxEventHandler.js     1.updateCheckBoxStatus 함수 추가 안되던 부분 수정(globalStore가 객체 안의 배열 안의 객체인 구조이기 때문에 addTodoItemEventHandler.js처럼 checkBox 상태가 수정된 새로운 Todos를 만들어 setStore 해줘야 함    Issus:정렬을 다시 해줘야 하는데 하나의 기능 모듈에서 현재 주소를 받아 주소에 따라 정렬을 다르게 해줘도 되는지 찾아보는 중

--- src/features/changeCheckBoxEventHandler.js
+++ src/features/changeCheckBoxEventHandler.js
@@ -1,28 +1,31 @@
 import { globalStore } from "../stores/globalStore";
 
 //수정하기 전에 id를 이용해 해당 객체의 isDone 값을 가져오고 그 값이 Y면 N을, N이면 Y을 저장하는 객체 생성
-const getNewIsDoneById = (id) => {
-  const todo = globalStore.getState().posts;
-  const target = todo.find((el) => el.id === Number(id));
+const getUpdatedTodoForCheckBoxById = (todos, id) => {
+  const target = todos.find((el) => el.id === Number(id));
 
   const newIsDone = target.isDone === "Y" ? "N" : "Y";
   return { ...target, isDone: newIsDone }; //객체 병합
 };
 
 //수정
-const updateCheckBoxStatus = (updateIsDoneTarget) => {
-  globalStore.setState(updateIsDoneTarget);
+const updateCheckBoxStatus = (todos, updatedTodo) => {
+  const updatedTodos = todos.map((todo) =>
+    todo.id === updatedTodo.id ? updatedTodo : todo
+  );
+  globalStore.setState({ posts: updatedTodos });
 };
 
 const changeCheckBoxEventHandler = (e) => {
   console.log("changeCheckBox 동작");
+  const todos = globalStore.getState().posts;
   const id = e.target
     .closest(".todo")
     .querySelector('input[type="hidden"]').value;
 
-  const updateIsDoneTarget = getNewIsDoneById(id);
-  console.log(updateIsDoneTarget);
-  updateCheckBoxStatus(updateIsDoneTarget);
+  const updatedTodo = getUpdatedTodoForCheckBoxById(todos, id);
+  // console.log(updatedTodo);
+  updateCheckBoxStatus(todos, updatedTodo);
   console.log(globalStore.getState().posts);
 };
 export default changeCheckBoxEventHandler;
